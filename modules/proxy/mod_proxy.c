a130463e5cdf2d048b0e4898bf4a74f76d8515c6
Lubomir Rintel <lubo.rintel@gooddata.com>
2014-02-17 21:45:05 +0100

    mod_proxy: Split the NoProxy parameter parsing away
    It is nice and could be reused by ProxyBlock.

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -1740,6 +1740,60 @@ static const char* cookie_domain(cmd_parms *cmd, void *dconf, const char *f,
     return NULL;
 }
 
+/* Add an entry to a NoProxy or ProxyBlock list */
+const char *
+    add_exclude_list(cmd_parms *parms, const char *arg, apr_array_header_t *array)
+{
+    struct exclude_entry *New;
+    struct exclude_entry *list = (struct exclude_entry *) array->elts;
+    int found = 0;
+    int i;
+
+    /* Don't duplicate entries */
+    for (i = 0; i < array->nelts; i++) {
+        if (strcasecmp(arg, list[i].name) == 0) {
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        New = apr_array_push(array);
+        New->name = apr_pstrdup(parms->pool, arg);
+        New->hostaddr = NULL;
+
+        if (ap_proxy_is_ipaddr(New, parms->pool)) {
+#if DEBUGGING
+            ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
+                         "Parsed addr %s", inet_ntoa(New->addr));
+            ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
+                         "Parsed mask %s", inet_ntoa(New->mask));
+#endif
+        }
+        else if (ap_proxy_is_domainname(New, parms->pool)) {
+            ap_str_tolower(New->name);
+#if DEBUGGING
+            ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
+                         "Parsed domain %s", New->name);
+#endif
+        }
+        else if (ap_proxy_is_hostname(New, parms->pool)) {
+            ap_str_tolower(New->name);
+#if DEBUGGING
+            ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
+                         "Parsed host %s", New->name);
+#endif
+        }
+        else {
+            ap_proxy_is_word(New, parms->pool);
+#if DEBUGGING
+            fprintf(stderr, "Parsed word %s\n", New->name);
+#endif
+        }
+    }
+    return NULL;
+}
+
 static const char *
     set_proxy_exclude(cmd_parms *parms, void *dummy, const char *arg)
 {
@@ -1783,54 +1837,8 @@ static const char *
     server_rec *s = parms->server;
     proxy_server_conf *conf =
     ap_get_module_config(s->module_config, &proxy_module);
-    struct exclude_entry *New;
-    struct exclude_entry *list = (struct exclude_entry *) conf->dirconn->elts;
-    int found = 0;
-    int i;
 
-    /* Don't duplicate entries */
-    for (i = 0; i < conf->dirconn->nelts; i++) {
-        if (strcasecmp(arg, list[i].name) == 0) {
-            found = 1;
-            break;
-        }
-    }
-
-    if (!found) {
-        New = apr_array_push(conf->dirconn);
-        New->name = apr_pstrdup(parms->pool, arg);
-        New->hostaddr = NULL;
-
-        if (ap_proxy_is_ipaddr(New, parms->pool)) {
-#if DEBUGGING
-            ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
-                         "Parsed addr %s", inet_ntoa(New->addr));
-            ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
-                         "Parsed mask %s", inet_ntoa(New->mask));
-#endif
-        }
-        else if (ap_proxy_is_domainname(New, parms->pool)) {
-            ap_str_tolower(New->name);
-#if DEBUGGING
-            ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
-                         "Parsed domain %s", New->name);
-#endif
-        }
-        else if (ap_proxy_is_hostname(New, parms->pool)) {
-            ap_str_tolower(New->name);
-#if DEBUGGING
-            ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
-                         "Parsed host %s", New->name);
-#endif
-        }
-        else {
-            ap_proxy_is_word(New, parms->pool);
-#if DEBUGGING
-            fprintf(stderr, "Parsed word %s\n", New->name);
-#endif
-        }
-    }
-    return NULL;
+    return add_exclude_list(parms, arg, conf->dirconn);
 }
 
 static const char *
